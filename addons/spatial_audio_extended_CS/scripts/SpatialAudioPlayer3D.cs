cb4cae1a085dd3edc0ccf720754bfaaca3cf801b
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2026-05-29 19:05:38 -0600

    REWRITE: SpatialAudioPlayer.cs - Signals Completed

--- addons/spatial_audio_extended_CS/scripts/SpatialAudioPlayer3D.cs
+++ addons/spatial_audio_extended_CS/scripts/SpatialAudioPlayer3D.cs
@@ -7,9 +7,159 @@ namespace SpatialAudioCS;
 [Tool, Icon("uid://diaqpjedywsus"), GlobalClass]
 public partial class SpatialAudioPlayer3D : AudioStreamPlayer3D
 {
-	#region Signals
+	#region Signals - Zones
 
-	// TODO: Signals
+	/// <summary>
+	/// Emitted when a listener enters the inner (full-volume) radius.
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void InnerRadiusEnteredEventHandler(Node3D listener);
+
+	/// <summary>
+	/// Emitted when a listener leaves the inner (full-volume) radius.
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void InnerRadiusExitedEventHandler(Node3D listener);
+
+	/// <summary>
+	/// Emitted when a listener enters the falloff zone (between inner and outer).
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void FalloffZoneEnteredEventHandler(Node3D listener);
+
+	/// <summary>
+	/// Emitted when a listener exits the falloff zone (between inner and outer).
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void FalloffZoneExitedEventHandler(Node3D listener);
+
+	/// <summary>
+	/// Emitted when a listener enters the outer boudary.
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void AttenuationZoneEnteredEventHandler(Node3D listener);
+
+	/// <summary>
+	/// Emitted when a listener exits the outer boudary.
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void AttenuationZoneExitedEventHandler(Node3D listener);
+
+	#endregion
+
+	#region Signals - Occulusion
+
+	/// <summary>
+	/// Emitted when occlusion parameters change.
+	/// </summary>
+	/// <param name="wallCount">Number of walls involved.</param>
+	/// <param name="cutoffHz">Only frequencies below this value can pass through to the listener.</param>
+	[Signal] public delegate void OcclusionChangedEventHandler(int wallCount, float cutoffHz);
+
+	/// <summary>
+	/// Emitted when the audio becomes occluded by one or more walls.
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	/// <param name="wallCount">Number of walls involved.</param>
+	[Signal] public delegate void AudioOccludedEventHandler(Node3D listener, int wallCount);
+
+	/// <summary>
+	/// Emitted when occlusion clears and the audio becomes unoccluded.
+	/// </summary>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void AudioUnoccludedEventHandler(Node3D listener);
+
+	/// <summary>
+	/// Emitted when an occlusion ray collides with a surface.
+	/// </summary>
+	/// <param name="hitPosition"></param>
+	/// <param name="fromPosition"></param>
+	/// <param name="collider"></param>
+	/// <param name="listener">The object that is receiving sound.</param>
+	[Signal] public delegate void OcclusionRayCollidedEventHandler(Vector3 hitPosition, Vector3 fromPosition, Node collider, Node listener);
+
+	#endregion
+
+	#region Signals - Reverb
+
+	/// <summary>
+	/// Emitted when reverb targets change.
+	/// </summary>
+	/// <param name="roomSize"></param>
+	/// <param name="wetness">The ammount of reverb applied. 
+	/// <para>Dry = Raw<para>Wet = Full</para><c>0.0 - 1.0</c></para></param>
+	/// <param name="damping">How quickly high frequncies decay relative to low frequencies.
+	/// <para>Dark/Warm = Highs fade faster <para>Bright/Airy = Highs persist with lows</para><c>0.0 - 1.0</c></para></param>
+	[Signal] public delegate void ReverbUpdatedEventHandler(float roomSize, float wetness, float damping);
+
+	/// <summary>
+	/// Emitted when room size or weness changes (higher-level reverb zone change).
+	/// </summary>
+	/// <param name="roomSize"></param>
+	/// <param name="wetness">The ammount of reverb applied. 
+	/// <para>Dry = Raw<para>Wet = Full</para><c>0.0 - 1.0</c></para></param>
+	[Signal] public delegate void ReverbZoneChangedEventHandler(float roomSize, float wetness);
+
+	/// <summary>
+	/// Emitted when a reverb/reflection ray collides with a surface.
+	/// </summary>
+	/// <param name="hitPosition"></param>
+	/// <param name="fromPosition"></param>
+	/// <param name="collider"></param>
+	[Signal] public delegate void ReverbRayCollidedEventHandler(Vector3 hitPosition, Vector3 fromPosition, Node collider);
+
+	#endregion
+
+	#region Signals - Air Absorption
+
+	/// <summary>
+	/// Emitted when air-absorption lowpass cutoff changes.
+	/// </summary>
+	/// <param name="cutoffHz">Only frequencies below this value can pass through to the listener.</param>
+	[Signal] public delegate void AirAbsorptionUpdatedEventHandler(float cutoffHz);
+
+	/// <summary>
+	/// Emitted when the air-absorption zone changes (min/max thresholds crossed).
+	/// </summary>
+	/// <param name="cutoffHz">Only frequencies below this value can pass through to the listener.</param>
+	[Signal] public delegate void AirAbsorptionZoneChangedEventHandler(float cutoffHz);
+
+	#endregion
+
+	#region Signals - Playback
+
+	/// <summary>
+	/// Emitted periodically when listener distance changes significantly.
+	/// </summary>
+	/// <param name="distance"></param>
+	[Signal] public delegate void ListenerDistanceChangedEventHandler(float distance);
+
+	/// <summary>
+	/// Emitted when playback actually starts (immediate or deferred).
+	/// </summary>
+	[Signal] public delegate void SpatialAudioPlaybackStartedEventHandler();
+
+	/// <summary>
+	/// Emitted when playback is stopped.
+	/// </summary>
+	[Signal] public delegate void SpatialAudioPlaybackStoppedEventHandler();
+
+	#endregion
+
+	#region Signals - Debug
+
+	/// <summary>
+	/// Emitted when the debug overlay visibility is toggled.
+	/// </summary>
+	/// <param name="visible"></param>
+	[Signal] public delegate void DebugOverlayToggledEventHandler(bool visible);
+
+	/// <summary>
+	/// Emits a compact diagnostics dictionary when the debug overlay is shown.
+	/// <para>Keys: <c>Distance</c>, <c>VolumeDbTarget</c>, <c>LowpassCutoff</c>, <c>ReverbRoomSize</c>, <c>WallCount</c></para>
+	/// </summary>
+	/// <param name="info">Keys: <c>Distance</c>, <c>VolumeDbTarget</c>, <c>LowpassCutoff</c>, <c>ReverbRoomSize</c>, <c>WallCount</c></param>
+	[Signal] public delegate void SpatialAudioDebugInfoEventHandler(Godot.Collections.Dictionary info);
 
 	#endregion
 
@@ -32,7 +182,7 @@ public partial class SpatialAudioPlayer3D : AudioStreamPlayer3D
 	#endregion
 
 	#region Lifecycle
-	
+
 	// TODO: Lifecycle
 
 	#endregion
